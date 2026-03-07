86ca130bb3e4e512b051b9ddb5f5a73e0fe0946c
Dylan5214 <dylan23719@gmail.com>
2026-03-06 22:25:10 -0500

    Displays data in dict format from API on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react"
 
 function App() {
+  const [sensors, setSensors] = useState([])
+
+  useEffect(() => {
+    const es = new EventSource("http://localhost:8000/stream")
+  
+    es.onmessage = (e) => {
+    setSensors(JSON.parse(e.data))
+  }
+
+  return () => es.close()
+}, [])
   return (
     <div className="dashboard">
-      <h1>NodeWatch</h1>
+    <h1>NodeWatch</h1>
+    <pre>{JSON.stringify(sensors, null, 2)}</pre>
     </div>
   )
 }
